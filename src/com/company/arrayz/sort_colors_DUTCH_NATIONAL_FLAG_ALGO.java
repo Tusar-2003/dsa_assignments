f1300d3060491c808c078dadbfb86c937b20ba14
Tusar Manna <tusarmanna2003@gmail.com>
2022-06-13 19:08:11 +0530

    13/06/2022 New Algo

---
+++ src/com/company/arrayz/sort_colors_DUTCH_NATIONAL_FLAG_ALGO.java
@@ -0,0 +1,43 @@
+package com.company.arrayz;
+//Sort Colors
+//DUTCH NATIONAL FLAG ALGORITHM
+//https://leetcode.com/problems/sort-colors/
+//https://www.youtube.com/watch?v=oaVa-9wmpns
+import java.util.Arrays;
+
+public class sort_colors_DUTCH_NATIONAL_FLAG_ALGO {
+    public static void main(String[] args) {
+        int a[]={2,0,2,1,1,0};
+        sortColors(a);
+
+    }
+    public static void sortColors(int[] nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        int temp=0;
+        while(mid<= high)
+        {
+            if(nums[mid]==0)
+            {
+                temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
