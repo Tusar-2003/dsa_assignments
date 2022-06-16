16ab2fbe65fd97a4798e05919ef96d33bd4f8550
Tusar Manna <tusarmanna2003@gmail.com>
2022-06-16 19:30:25 +0530

    16/06/2022

---
+++ src/com/company/sorting/find_peak_element.java
@@ -0,0 +1,27 @@
+package com.company.sorting;
+//find peak element in a zig zag array
+//https://leetcode.com/problems/find-peak-element/
+//https://www.youtube.com/watch?v=W9QJ8HaRvJQ&t=3732s
+//same as mountain array
+public class find_peak_element {
+    public static void main(String[] args) {
+        int a[]={1,2,3,1};
+        int result=findPeakElement(a);
+        System.out.println(result);
+    }
+    public static int findPeakElement(int[] arr) {
+        int start=0,end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1])
+            {
+                end=mid;
+
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
